c8a53128dbced43a465b48ddb668eeefbdf7fb16
Gregory Blickley <gregoryblickley@gdcit.com>
2024-05-10 18:17:25 -0400

    Added in echo's

--- app_installer.sh
+++ app_installer.sh
@@ -3,26 +3,33 @@
 #Variables
 appName="SonicWall Mobile Connect.app"
 tempDir="$HOME/Downloads/SonicWall-Temp"
-$gitInst="https://github.com/GregBlickley/SonicWall-MacOS/blob/main/app_installer.sh"
+$gitInst="https://github.com/GregBlickley/SonicWall-MacOS/raw/main/Sonicwall_VD.dmg"
 $dmgFile="Sonicwall_VD.dmg"
 $dmgName="Sonicwall_VD"
 
 # download .dmg
+echo "downloading application file"
 curl -sSl "$gitInst" -o "$tempDir/$dmgFile"
 
 # confirm download of .dmg
 if [ $? != 0 ]; then
+    echo "download failed."
     exit 1
 fi
+echo "download successful."
 
 # mount the .dmg
+echo "attaching to virtual drive"
 hdiutil attach "$tempDir/$dmgFile"
 
 # move application stored in .dmg to applications
+echo "moving application contents"
 mv "/Volumes/$dmgName/$appName"
 
 # unmount the .dmg
+echo "detaching from virtual drive"
 hdiutil detach "/Volumes/$dmgName"
 
 # remove the .dmg file from device
+echo "removing excess install files."
 rm -r "$tempDir/$dmgFile"
--- download_installer.sh
+++ download_installer.sh
@@ -3,8 +3,7 @@
 #Variables
 appName="SonicWall Mobile Connect.app"
 tempDir="$HOME/Downloads/SonicWall-Temp"
-$gitInst="https://github.com/GregBlickley/SonicWall-MacOS/blob/main/download_installer.sh"
-
+$gitInst="https://raw.githubusercontent.com/GregBlickley/SonicWall-MacOS/main/app_installer.sh"
 # check to see if the application exists
 if ls /Application | grep -qi "$appName"; then
 
@@ -13,29 +12,36 @@ if ls /Application | grep -qi "$appName"; then
 
 # else
 else    
+    echo "App not installed"
     
     # configure temporary directory
     mkdir -p "$tempDir"
-    
+    echo "Temporary directory made."
+
     # download the file from GitHub
+    echo "downloading installer.."
     curl -sSl "$gitInst" -o "$tempDir/installer.sh"
 
     # check if the download was successful
     if [ $? != 0 ]; then
+        echo "download failed."
         exit 1                  # exit 1 if the download failed
     fi
 
     # Run the install.sh
+    echo "running installer."
     source "$tempDir/installer.sh"
 
     # wait for the script to finish
     tmp="How do we do this?"
 
     # Remove the install.sh file
+    echo "removing temp directory"
     rm -r "$tempDir"
 
     # verify that the application was installed
     if ls /Application | grep -qi "$appName"; then
+        echo "application has been installed."
         exit 0
     else
         exit 1
